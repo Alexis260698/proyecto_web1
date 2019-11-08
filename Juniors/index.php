3eb7d7efc6332bac7b7dcd62beaa32ab55fbfc90
Alexis Hernandez <44562141+Alexis260698@users.noreply.github.com>
2019-11-07 19:27:49 -0600

    nabvar
    Navbar

--- Juniors/index.php
+++ Juniors/index.php
@@ -1,19 +1,4 @@
-<?php
-
-
- ?>
-
-
- <!DOCTYPE html>
- <html lang="en" dir="ltr">
-   <head>
-     <meta charset="utf-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-     <link rel="stylesheet" href="css/bootstrap.css">
-     <link rel="stylesheet" href="css/index.css">
-
-     <title>Juniors Gym</title>
+<?php? include('navbar.php')>
 
      <div class="jumbotron">
 
