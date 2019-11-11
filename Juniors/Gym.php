a37b038da2cee82cc0a43b4b9f5a3b13eb4c222e
MIGUELANGELSERNAPEREZ <55261893+MIGUELANGELSERNAPEREZ@users.noreply.github.com>
2019-11-11 08:42:22 -0600

    se creo toda la clase gym.php y gym.css

---
+++ Juniors/Gym.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Junniors Gym</title>
+    <link rel="stylesheet" href="css/Gym.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+   <script type="text/javascript" src="js/bootstrap.min.js"> </script>
+   <script type="text/javascript" src="jquery-3.4.1.min.js"></script>
+
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    			<a href="#" class="navbar-brand" id="titulo"> Junniors GYM</a>
+    			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-Menu" aria-controls="navbar-Menu"	aria-expanded="false">
+    					<span class="navbar-toggler-icon"></span>
+    			</button>
+
+    			<div class="collapse navbar-collapse" id="navbar-Menu">
+    						<ul class="navbar-nav mr-auto">
+    										<li class="nav-item">
+    											 <a href="index.php" class="nav-link">Inicio</a>
+    										 </li>
+
+    										<li class="nav-item">
+    											 <a href="#" class="nav-link">Hacerca de</a>
+    										</li>
+
+                        <li class="nav-item">
+                           <a href="areaNiños.php" class="nav-link">Area para Niños</a>
+                        </li>
+
+    										<li class="nav-item dropdown">
+    											 <a href="#" class="nav-link dropdown-toggle" id="dropdownAsig" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    												 	Servicios
+    											 </a>
+
+    											<div class="dropdown-menu" aria-labelledby="dropdownAsig">
+    													<a href="#" class="dropdown-item"> GYM</a>
+    													<a href="#" class="dropdown-item"> Spinning</a>
+    													<a href="#" class="dropdown-item"> Artes Marsiales</a>
+    											</div>
+
+    										</li>
+    						</ul>
+
+    			</div>
+    </nav> <!--fin del menu-->
+
+<!--Inicio del slide-->
+    <div class="container">
+      <div class="col-md-12">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/logo.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block" style="background-color: black">
+            <h5>¡¡ Vive la Experiencia !!</h5>
+              <p>Se Parte de Junniors GYM.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="img/jaula.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="img/zonaMancuernas.jpg" class="d-block w-100" alt="...">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+      </div>
+
+    </div> <!--Fin del slide-->
+
+      <div class="container-Horario">
+        <div class="row">
+                <div class="col-sm-4 mt-4">
+                    <div class="card	bg-light">
+                      <div class="card-header	bg-primary text-white text-uppercase">
+                        Horarios
+
+                      </div>
+
+                      <div class="card-body">
+                          <h5>Lunes - Viernes</h5>
+                          <p>6:00 am - 10:30 pm</p>
+                          <h5>Sabado</h5>
+                          <p>6:00 am - 12:00 pm</p>
+                          <h5>Domingo</h5>
+                          <p>8:00 am - 12:00 pm</p>
+                      </div>
+                    </div>
+
+                </div> <!--Fin del formulario Horario-->
+                <div class="col">
+                  <div class="card border-primary mb-3" style="max-width: 60rem;">
+                      <div class="card-header">Descripción</div>
+                        <div class="card-body text-primary">
+                          <h5 class="card-title" style="color:back">Gym</h5>
+                            <p class="card-text">Contamos con los mejores aparatos del momento, todos hechos en fabrica, contamos con pesas, mancuernas,
+                               bancos,diversas maquinas, escaladoras, caminadoras, barras ect.</p>
+                        </div>
+                  </div>
+
+                </div>
+
+
+        </div>
+      </div>
+
+
+      <footer>
+
+         <div class="container-footer-all">
+
+              <div class="container-body">
+
+                  <div class="colum1">
+                      <h1>Mas informacion de la compañia</h1>
+
+                      <p>Esta compañia se dedica al desarrollo se software de software con un
+                      conjunto de cosas que no se lo que estoy escribiendo, este
+                      texto es solo para llenara informacion en el cuadro de informacion
+                      de la compañia.</p>
+
+                  </div>
+
+                  <div class="colum2">
+
+                      <h1>Redes Sociales</h1>
+
+                      <div class="row">
+                          <img src="icon/facebook.png">
+                          <label>Siguenos en Facebook</label>
+                      </div>
+                      <div class="row">
+                          <img src="icon/twitter.png">
+                          <label>Siguenos en Twitter</label>
+                      </div>
+                      <div class="row">
+                          <img src="icon/instagram.png">
+                          <label>Siguenos en Instagram</label>
+                      </div>
+                      <div class="row">
+                          <img src="icon/google-plus.png">
+                          <label>Siguenos en Google Plus</label>
+                      </div>
+                      <div class="row">
+                          <img src="icon/pinterest.png">
+                          <label>Siguenos en Pinteres</label>
+                      </div>
+
+
+                  </div>
+
+                  <div class="colum3">
+
+                      <h1>Informacion Contactos</h1>
+
+                      <div class="row2">
+                          <img src="icon/house.png">
+                          <label>La Romana,
+                          Republica Dominicana
+                          Manuel del Cabral
+                          Casa # 27</label>
+                      </div>
+
+                      <div class="row2">
+                          <img src="icon/smartphone.png">
+                          <label>+1-829-395-2064</label>
+                      </div>
+
+                      <div class="row2">
+                          <img src="icon/contact.png">
+                           <label>s16120271@alumnos.itsur.edu.mx</label>
+                      </div>
+
+                  </div>
+
+              </div>
+
+          </div>
+
+          <div class="container-footer">
+                 <div class="footer">
+                      <div class="copyright">
+                          © 2019 Todos los Derechos Reservados | <a href="">ITSUR</a>
+                      </div>
+
+                      <div class="information">
+                          <a href="">Informacion Compañia</a> | <a href="">Privacion y Politica</a> | <a href="">Terminos y Condiciones</a>
+                      </div>
+                  </div>
+
+              </div>
+
+      </footer>
+
+
+  </body>
+</html>
