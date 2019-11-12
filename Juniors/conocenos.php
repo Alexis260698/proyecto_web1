9bb89c6352b143872ae171407c26ffb5aac12983
Alexis Hernandez <44562141+Alexis260698@users.noreply.github.com>
2019-11-11 20:55:00 -0600

    CR2

--- Juniors/conocenos.php
+++ Juniors/conocenos.php
@@ -1,8 +1,18 @@
-<?php include('navbar.php');?>
+
 <link rel="stylesheet" type="text/css" href="css/Conocenos.css">
 
-<div class="jumbotron">
-	<div class="container">
-		
+<body>
+<div class="container">
+	<div class="row">
+		<div class="card">
+			<img src="img/conocenos.PNG" alt="error" class="card-img-top">
+		</div>
 	</div>
 </div>
+
+
+<div class="">
+	 <p class="font-weight-bolder">¿Donde nos encuentras?</p>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3746.1010100812173!2d-101.19173028560714!3d20.129833123232697!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842cfae8cedbdac3%3A0x5174eda8e92aefdd!2sJUNIOR&#39;S%20GYM%20MOROLEON!5e0!3m2!1ses-419!2smx!4v1573524499773!5m2!1ses-419!2smx" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+</div>
+</body>
